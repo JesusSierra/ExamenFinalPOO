8bfc05f1470c76826c196341ce615378ec97da13
Jesus Sierra <36081332+JesusSierra@users.noreply.github.com>
2019-05-16 12:35:07 -0600

    Add files via upload

---
+++ ExamenFinal_PtIII/ExamenFinal_PtIII/Classes/CMiExcepcion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenFinal_PtIII.Classes
+{
+    class CMiExcepcion : Exception
+    {
+        public CMiExcepcion()
+        {
+
+        }
+
+        public CMiExcepcion(string mensaje)
+        {
+
+        }
+
+        public CMiExcepcion (string mensaje2, Exception l)
+        {
+
+        }
+    }
+}
---
+++ ExamenFinal_PtIII/ExamenFinal_PtIII/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamenFinal_PtIII.Classes;
+
+namespace ExamenFinal_PtIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string apellido;
+            int numeroLetras = 0;
+            int numeroLetras2 = 0;
+
+            Console.WriteLine("Ingrese su nombre");
+            nombre = Console.ReadLine();
+            numeroLetras = nombre.Length;
+            Console.WriteLine("Ingrese su apellido");
+            apellido = Console.ReadLine();
+            numeroLetras2 = apellido.Length;
+
+            try
+            {
+                if (numeroLetras <= 2)
+                {
+                    throw new CMiExcepcion();
+                }if (numeroLetras2 <= 2)
+                {
+                    throw new CMiExcepcion();
+                }
+            }
+            catch (CMiExcepcion)
+            {
+                Console.WriteLine("Nombre/Apellido no válido");
+            }
+        }
+    }
+}
